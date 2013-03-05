6b8e54bfb59f2e63f8c84da44ad1d0b81836b69b
Matthias Reining <matthias.reining@gmx.net>
2013-03-05 08:15:59 +0100

    move winstone (classpath conflicts)

--- src/main/java/com/sourcecoding/blog/business/build/control/CopyingMachine.java
+++ src/main/java/com/sourcecoding/blog/business/build/control/CopyingMachine.java
@@ -5,6 +5,9 @@
 package com.sourcecoding.blog.business.build.control;
 
 import java.io.*;
+import java.nio.file.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -22,33 +25,41 @@ public class CopyingMachine {
             File targetFile = new File(targetDir.getAbsolutePath() + File.separator + sourceFile.getName());
 
             if (sourceFile.isDirectory()) {
-                if (!targetFile.exists()) {
+                if (!targetFile.exists())
                     targetFile.mkdir();
-                }
                 copyDirectory(sourceFile, targetFile);
-            } else {
+            } else
                 copyFile(sourceFile, targetFile);
-            }
         }
     }
 
-    public void copyFile(File inputFile, File outputFile) {
+    public void copyFile(File sourceFile, File targetFile) {
+        //thx to Fabi (http://disq.us/8cga2n)
+        Path sourcePath = FileSystems.getDefault().getPath(sourceFile.getAbsolutePath());
+        Path destPath = FileSystems.getDefault().getPath(targetFile.getAbsolutePath());
+        try {
+            Files.copy(sourcePath, destPath, StandardCopyOption.REPLACE_EXISTING,
+                    StandardCopyOption.COPY_ATTRIBUTES);
+        } catch (IOException ex) {
+            Logger.getLogger(CopyingMachine.class.getName()).
+                    log(Level.SEVERE, null, ex);
+        }
+    }
 
-        System.out.println("copy " + inputFile.getName() + " to " + outputFile.getAbsolutePath());
-
+    @Deprecated
+    public void copyFileJDK6(File inputFile, File outputFile) {
+        System.out.println("copy " + inputFile.getName() + " to " + 
+                outputFile.getAbsolutePath());
         try (FileInputStream fis = new FileInputStream(inputFile);
                 FileOutputStream fos = new FileOutputStream(outputFile)) {
-
-
             byte[] b = new byte[1024];
             int noOfBytes;
             while ((noOfBytes = fis.read(b)) != -1) {
                 fos.write(b, 0, noOfBytes);
             }
         } catch (IOException ex) {
-            ex.printStackTrace();
+             Logger.getLogger(CopyingMachine.class.getName()).
+                    log(Level.SEVERE, null, ex);
         }
-
-
     }
 }
